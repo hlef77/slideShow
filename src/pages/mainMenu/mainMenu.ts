2e83a66e950ccf0a838735a1c67c2183a50f6760
takami <hlef.wtbr.77@gmail.com>
2018-02-12 21:26:43 +0900

    ログインページからのパラメータ渡し例作成

--- src/pages/mainMenu/mainMenu.ts
+++ src/pages/mainMenu/mainMenu.ts
@@ -1,16 +1,34 @@
 import {
   Component,
+  OnInit
 } from '@angular/core';
+
+import {
+  Router,
+  ActivatedRoute,
+  ParamMap
+} from '@angular/router';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-main-menu-page',
   templateUrl: './mainMenu.html',
   styleUrls: ['./mainMenu.css']
 })
-export class MainMenuPageComponent {
+export class MainMenuPageComponent implements OnInit {
 
-  constructor() {
+  userKind: string;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap
+    .subscribe((params: ParamMap) => {
+      this.userKind = params.get('userKind');
+    });
   }
-
 }
